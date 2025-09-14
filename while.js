6554617deed80ccda9cc8d6ee0b35a951e8420f5
ashleysitdown <evanashley.ng@gmail.com>
2025-09-14 17:32:43 +0800

    added new line

--- while.js
+++ while.js
@@ -5,8 +5,17 @@ while (counter <= 3) {
 }
 
 let counter2 = 1;
-while (counter2 <=3) {
-    console.log('Merry Christmas' + ' Happy New Year');
+while (counter2 <=10){
+    console.log('Merry Christmas');
     counter2 = counter2 + 1;
+}
+
+
+
+
+let counter3 = 1;
+while (counter3 <=3) {
+    console.log('Merry Christmas' + ' Happy New Year');
+    counter3 = counter3 + 1;
 }
 
