4a4e742af07c0a310ea07a636ee785aa5f1de9f4
ashleysitdown <evanashley.ng@gmail.com>
2025-09-14 17:27:54 +0800

    new file

---
+++ while.js
@@ -0,0 +1,12 @@
+let counter = 1;
+while (counter <= 3) {
+    console.log('Merry Christmas');
+    counter = counter + 1;
+}
+
+let counter2 = 1;
+while (counter2 <=3) {
+    console.log('Merry Christmas' + ' Happy New Year');
+    counter2 = counter2 + 1;
+}
+
