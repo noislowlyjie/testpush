86b5604e7f3646f4d203258e0d14561ab4af219a
ashleysitdown <evanashley.ng@gmail.com>
2025-09-14 19:31:22 +0800

    added new lines

--- while.js
+++ while.js
@@ -19,3 +19,40 @@ while (counter3 <=3) {
     counter3 = counter3 + 1;
 }
 
+let counter4 = 1;
+while (counter4 <=5) {
+    console.log('Count: ' + counter4);
+    counter4 = counter4 + 1;
+}
+
+let counter5 = 2;
+while (counter5 <=10) {
+    console.log(counter5);
+    counter5 = counter5 + 2;
+}
+
+let counter6 = 20;
+while (counter6 >= 1) {
+    console.log(counter6);
+    counter6 = counter6 - 1;
+}
+
+let counter7 = 1;
+while (counter7 <= 1000) {
+    console.log(counter7);
+    counter7 = counter7 + 2;
+}
+
+let counter8 = 2;
+while (counter8 <=50) {
+    console.log(counter8);
+    counter8 = counter8 + 2;
+}
+
+let counter9 = 0;
+while (counter9 <=50) {
+    if (counter9 > 0)
+        console.log(counter9);
+    counter9 = counter9 + 2;
+}
+
